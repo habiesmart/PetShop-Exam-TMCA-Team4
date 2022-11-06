5a65aa6ae6b3d74c87b40fcbeac1a01d87ae2cf4
shirath <shirath24@gmail.com>
2022-11-05 19:52:59 -0800

    update table paket

--- database/migrations/2022_11_06_033159_create_paket_perawatans_table.php
+++ database/migrations/2022_11_06_033159_create_paket_perawatans_table.php
@@ -16,6 +16,7 @@ class CreatePaketPerawatansTable extends Migration
         Schema::create('paket_perawatans', function (Blueprint $table) {
             $table->id();
             $table->string("nama_paket");
+            $table->string("deskripsi_paket");
             $table->double("harga_paket");
             $table->timestamps();
         });
