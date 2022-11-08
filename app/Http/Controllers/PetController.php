1b1efd45cc55c74c94e8cb51010368f3817c5938
muhammad.budiman <habie.budiman@360consulting.co.id>
2022-11-08 10:48:28 +0700

    Add method get all save delete

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -7,15 +7,46 @@ use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
+    public function Index()
+    {
+        return view('Pet.index');
+    }
+    
     public function ListPet()
     {
         $pets = Pet::all();
-        return $pets;
+        $data = [
+            'status' => true,
+            'message' => 'success',
+            'data' => $pets
+        ];
+
+        return response()->json($data);
     }
 
     public function GetPet($id)
     {
         $pets = Pet::Where('id', '==');
         return $pets;
+    }
+
+    public function Save(Request $request)
+    {
+        $pet = [
+            "name" => $request->input('name'),
+            "born" => $request->input('born'),
+            "jenis_hewan" => $request->input('jenis_hewan'),
+            "aboutme" => $request->input('aboutme'),
+        ];
+        
+        Pet::create($pet);
+        return redirect()->to('pet');
+    }
+
+    public function Delete($id)
+    {
+        $pet = Pet::find($id);
+        $pet->delete();
+        return redirect()->to('pet');
     }
 }
